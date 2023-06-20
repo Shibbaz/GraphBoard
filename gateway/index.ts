a5595c0124f8d1ea2fec6069b3d41a0d68c8377b
Shibbaz <kmosc@protonmail.com>
2023-06-20 14:43:17 +0200

    feat: update gateway

--- gateway/index.ts
+++ gateway/index.ts
@@ -1,16 +1,26 @@
 const { ApolloServer } = require('apollo-server');
-const { ApolloGateway } = require('@apollo/gateway');
+const { ApolloGateway, RemoteGraphQLDataSource } = require('@apollo/gateway');
 const { readFileSync } = require('fs');
-
 const supergraphSdl = readFileSync('./supergraph.graphql').toString();
+const { ApolloLogPlugin } = require('apollo-log');
+const plugins = [ApolloLogPlugin];
+
 
 const gateway = new ApolloGateway({
     supergraphSdl,
-    
 });
 
 const server = new ApolloServer({
     gateway,
+    plugins,
+    context: ({ req }) => {
+        return {
+          serverRequest: req,
+          Headers: {
+            "Authorization": req.headers.authorization
+          }
+        };
+    }
 });
 
 server.listen().then(({ url }) => {
