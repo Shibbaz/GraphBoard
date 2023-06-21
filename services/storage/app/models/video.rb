8f98fc6c3e6e091e1e6ff479574b0e6370c9e43f
Shibbaz <kmosc@protonmail.com>
2023-06-21 16:47:36 +0200

    style: standardrb --fix

--- services/storage/app/models/video.rb
+++ services/storage/app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ApplicationRecord
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :type, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :type, presence: true
 end
