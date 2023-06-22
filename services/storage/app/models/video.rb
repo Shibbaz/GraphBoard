764fa42480ba6ceff69d08b7f26c52c4b95a605d
Shibbaz <kmosc@protonmail.com>
2023-06-22 02:11:54 +0200

    feat: add validations to Videos

--- services/storage/app/models/video.rb
+++ services/storage/app/models/video.rb
@@ -1,5 +1,8 @@
 class Video < ApplicationRecord
   validates :name, presence: true
+  validates :author, presence: true
+  validates :rules, presence: true
+  validates :id, presence: true
   validates :description, presence: true
   validates :type, presence: true
 end
