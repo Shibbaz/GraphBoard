49f9193ce091b6cb5222ac011fc7781242ecf8f2
Shibbaz <kmosc@protonmail.com>
2023-06-23 18:16:16 +0200

    style: linter

--- services/storage/app/graphql/types/user_type.rb
+++ services/storage/app/graphql/types/user_type.rb
@@ -1,13 +1,12 @@
 module Types
-    class UserType < Types::BaseObject
-      key fields: ['id']
-      extend_type
-      field :id, ID, null: false, external: true
-      field :videos, Types::VideoType.connection_type, null: true
-  
-      def videos
-        Video.where(author: object[:id])
-      end
-      
+  class UserType < Types::BaseObject
+    key fields: ["id"]
+    extend_type
+    field :id, ID, null: false, external: true
+    field :videos, Types::VideoType.connection_type, null: true
+
+    def videos
+      Video.where(author: object[:id])
     end
   end
+end
--- services/storage/app/graphql/types/video_type.rb
+++ services/storage/app/graphql/types/video_type.rb
@@ -5,13 +5,13 @@ module Types
     field :video_type, String, null: true
     field :rules, GraphQL::Types::JSON, null: true
     field :user, Types::UserType, null: true
-  
+
     def self.resolve_reference(object, _context)
       Article.where(user_id: object[:id])
     end
-    
+
     def user
-      { __typename: 'User', id: object[:author] }
+      {__typename: "User", id: object[:author]}
     end
   end
 end
