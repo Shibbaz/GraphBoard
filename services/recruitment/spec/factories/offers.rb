80d4f02b09ff2bbdc16f9ba8e0d09328bebea5ea
Shibbaz <kmosc@protonmail.com>
2023-06-25 18:24:06 +0200

    fix: factory offers

--- services/recruitment/spec/factories/offers.rb
+++ services/recruitment/spec/factories/offers.rb
@@ -2,17 +2,22 @@ FactoryBot.define do
   factory :offer do
     name { "Ruby Developer" }
     description { "This is offer for Ruby developer MID level" }
-    requirements { {
-        technology: name,
+    requirements { [
+      details: {
+        technology: Faker::ProgrammingLanguage.name,
         experience: 10,
         seniority: "MID",
+     },
+      work_environment: {
         remote: true,
         office: true,
         hybrid: true
-     } 
+      }
+    ]
     }
     tags { 
-      [create(:technology_tag, name: Faker::ProgrammingLanguage.name).id]
+      tag = create(:technology_tag)
+      [tag.id]
     }
     author { SecureRandom.uuid }
     contact_details {
