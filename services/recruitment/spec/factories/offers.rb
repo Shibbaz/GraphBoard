38ba330fb01a356f530b499e986a0a9625e5b28a
Shibbaz <kmosc@protonmail.com>
2023-06-25 17:31:19 +0200

    test: extend offers factories

--- services/recruitment/spec/factories/offers.rb
+++ services/recruitment/spec/factories/offers.rb
@@ -1,22 +1,19 @@
-name = Faker::ProgrammingLanguage.name
 FactoryBot.define do
   factory :offer do
-    name { "#{name} Developer" }
-    description { "This is offer for #{name} developer MID level" }
-    requirements { [{
-      technologies: [{
+    name { "Ruby Developer" }
+    description { "This is offer for Ruby developer MID level" }
+    requirements { {
         technology: name,
         experience: 10,
-        seniority: "MID"
-      }],
-      type: {
+        seniority: "MID",
         remote: true,
         office: true,
         hybrid: true
-      }
-      }
-      ] }
-    tags { create(:technology_tag, name: name) }
+     } 
+    }
+    tags { 
+      [create(:technology_tag, name: Faker::ProgrammingLanguage.name).id]
+    }
     author { SecureRandom.uuid }
     contact_details {
       {
@@ -25,6 +22,9 @@ FactoryBot.define do
         recruiter_name: Faker::Name.name,
         email: Faker::Internet.email
       }
-     }
+    }
+    candidates{
+      []
+    }
   end
 end
