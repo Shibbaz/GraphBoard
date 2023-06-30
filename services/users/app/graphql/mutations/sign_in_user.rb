195b2c45fe2ad15685b6922a39f661ec33c75df1
Shibbaz <kmosc@protonmail.com>
2023-07-01 01:34:14 +0200

    style: move sign in out off mutation

--- services/users/app/graphql/mutations/sign_in_user.rb
+++ services/users/app/graphql/mutations/sign_in_user.rb
@@ -6,17 +6,9 @@ module Mutations
         field :user, Types::UserType, null: true
 
         def resolve(credentials: nil)
-            return unless credentials
-
-            user = User.find_by email: credentials[:email]
-
-            return unless user
-            return unless user.authenticate(credentials[:password])
-
-            crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
-            token = crypt.encrypt_and_sign("user-id:#{user.id}")
-            context[:session][:token] = token
-            { user:, token: }
+            data = Concepts::Users::Queries::SignInUser.call(credentials)
+            context[:session][:token] = data[:token]
+            return data
         end
     end
 end
