8b83f3011b0a548ef3927258e045fbfbc66b4ce5
Shibbaz <kmosc@protonmail.com>
2023-07-02 01:32:09 +0200

    fix: error handling

--- services/storage/spec/mutations/update_video_spec.rb
+++ services/storage/spec/mutations/update_video_spec.rb
@@ -1,23 +1,32 @@
 RSpec.describe Mutations::UpdateVideo, type: :request do
   let(:video) { create(:video) }
-
+  let(:quote) { Faker::JapaneseMedia::OnePiece.quote }
+  let(:informations) {
+    {
+      name: "Two Piece",
+      description: quote,
+      video_type: "video"
+    }
+  }
   describe "Mutation Success" do
     it "expects to update user successfully" do
-      quote = Faker::JapaneseMedia::OnePiece.quote
       mutation = Mutations::UpdateVideo.new(object: nil, field: nil, context: {
         current_user_id: SecureRandom.uuid
       }).resolve(
         video_id: video.id,
-        informations: {
-          name: "Two Piece",
-          description: quote,
-          video_type: "video"
-        }
+        informations: informations
       )
       expect(Video.first.description).to eq(quote)
       expect(Video.first.name).to eq("Two Piece")
 
       expect(mutation[:status]).to eq(200)
+    end
+
+    it "expects not to update video, video does not exist" do
+      random_video_id = SecureRandom.uuid
+      expect{Mutations::UpdateVideo.new(object: nil, field: nil, context: {
+        current_user_id: SecureRandom.uuid,
+      }).resolve(video_id: random_video_id, informations: informations)}.to raise_error(GraphQL::ExecutionError, "ActiveRecord::RecordNotFound")
     end
 
     it "expects authentication to fail" do
